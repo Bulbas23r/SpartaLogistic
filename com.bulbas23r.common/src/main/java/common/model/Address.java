15999bc9d7a87bbcf2297018e7b87ae5b7af4c50
juhee99 <eee6767@naver.com>
2025-03-17 14:40:19 +0900

    feat: Address 생성자 Builder 추가

--- com.bulbas23r.common/src/main/java/common/model/Address.java
+++ com.bulbas23r.common/src/main/java/common/model/Address.java
@@ -3,6 +3,7 @@ package common.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,6 +27,7 @@ public class Address {
     @Column(nullable = false)
     private String postalCode;
 
+    @Builder
     public Address(String roadAddress, String jibunAddress, String city, String district,
         String town, String postalCode) {
         validateNotEmpty(roadAddress, "도로명 주소(roadAddress)");
