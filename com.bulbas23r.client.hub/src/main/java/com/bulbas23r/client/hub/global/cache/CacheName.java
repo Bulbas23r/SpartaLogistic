59ebf3d960dd8a04498cd1f3e7ea72c10f67cc61
sinryuji <siler2474@gmail.com>
2025-03-20 18:21:12 +0900

    feat: hub caching

---
+++ com.bulbas23r.client.hub/src/main/java/com/bulbas23r/client/hub/global/cache/CacheName.java
@@ -0,0 +1,8 @@
+package com.bulbas23r.client.hub.global.cache;
+
+public class CacheName {
+
+    public static final String HUB = "hubCache";
+    public static final String HUB_LIST = "hubListCache";
+    public static final String HUB_SEARCH = "hubSearchCache";
+}
