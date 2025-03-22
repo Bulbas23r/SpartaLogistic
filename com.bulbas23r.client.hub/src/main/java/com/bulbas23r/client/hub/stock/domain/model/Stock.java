0fd2d712eda189c24aca9ad845a169b81866e863
sinryuji <siler2474@gmail.com>
2025-03-22 21:39:34 +0900

    feat: stock entity

---
+++ com.bulbas23r.client.hub/src/main/java/com/bulbas23r/client/hub/stock/domain/model/Stock.java
@@ -0,0 +1,26 @@
+package com.bulbas23r.client.hub.stock.domain.model;
+
+import common.model.BaseEntity;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLRestriction("is_deleted is false")
+@Table(name = "p_stock")
+public class Stock extends BaseEntity {
+
+    @EmbeddedId
+    private StockId id;
+
+    private Integer quantity;
+}
