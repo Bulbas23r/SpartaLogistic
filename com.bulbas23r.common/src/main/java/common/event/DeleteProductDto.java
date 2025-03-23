172c9f8bff7b8764cbda81386eb2f443d81ba03b
sinryuji <siler2474@gmail.com>
2025-03-23 16:42:55 +0900

    feat: DeleteProductDto

---
+++ com.bulbas23r.common/src/main/java/common/event/DeleteProductDto.java
@@ -0,0 +1,14 @@
+package common.event;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteProductDto {
+
+    private UUID productId;
+}
