2085359b7ff917231d48e06d13e9258a98dba2dd
sinryuji <siler2474@gmail.com>
2025-03-23 21:37:17 +0900

    feat: event dto

---
+++ com.bulbas23r.common/src/main/java/common/event/CreateOrderEventDto.java
@@ -0,0 +1,22 @@
+package common.event;
+
+
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateOrderEventDto {
+
+    UUID orderId;
+    UUID hubId;
+    List<OrderProductEventDto> products;
+}
