c5c1ca793f739ab22bd28a5bd589871d5ba94869
sinryuji <siler2474@gmail.com>
2025-03-24 11:26:42 +0900

    feat: delete product listner

--- com.bulbas23r.client.hub/src/main/java/com/bulbas23r/client/hub/stock/infrastructure/messaging/ProductEventConsumer.java
+++ com.bulbas23r.client.hub/src/main/java/com/bulbas23r/client/hub/stock/infrastructure/messaging/ProductEventConsumer.java
@@ -1,7 +1,9 @@
 package com.bulbas23r.client.hub.stock.infrastructure.messaging;
 
+import com.bulbas23r.client.hub.hub.application.service.HubService;
 import com.bulbas23r.client.hub.stock.application.service.StockService;
 import common.event.CreateStockEventDto;
+import common.event.DeleteProductDto;
 import common.event.TopicName;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -13,9 +15,15 @@ import org.springframework.stereotype.Service;
 public class ProductEventConsumer {
 
     private final StockService stockService;
+    private final HubService hubService;
 
     @KafkaListener(topics = TopicName.CREATE_PRODUCT)
     public void createStock(@Payload CreateStockEventDto event) {
         stockService.createStock(event);
+    }
+
+    @KafkaListener(topics = TopicName.DELETE_PRODUCT)
+    public void deleteStock(@Payload DeleteProductDto event) {
+        hubService.deleteStocksByProductId(event.getProductId());
     }
 }
