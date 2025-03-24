227db8274b272a49944da2839617cbdde2f12af3
zzangkkmin <zzangkkmin@naver.com>
2025-03-24 14:35:36 +0900

    fix: FeignClient 호출부분 수정

--- com.bulbas23r.client.product/src/main/java/com/bulbas23r/client/product/application/service/ProductServiceImpl.java
+++ com.bulbas23r.client.product/src/main/java/com/bulbas23r/client/product/application/service/ProductServiceImpl.java
@@ -6,14 +6,12 @@ import com.bulbas23r.client.product.domain.repository.ProductRepository;
 import com.bulbas23r.client.product.infrastructure.Const.ProductString;
 import com.bulbas23r.client.product.infrastructure.client.CompanyClient;
 import com.bulbas23r.client.product.infrastructure.client.HubClient;
-import com.bulbas23r.client.product.presentation.dto.ProductCompanyResponseDto;
 import com.bulbas23r.client.product.infrastructure.messaging.ProductEventProducer;
 import com.bulbas23r.client.product.presentation.dto.ProductCreateRequestDto;
-import com.bulbas23r.client.product.presentation.dto.ProductHubResponseDto;
 import com.bulbas23r.client.product.presentation.dto.ProductUpdateRequestDto;
-import common.utils.PageUtils;
+import common.dto.CompanyInfoResponseDto;
+import common.dto.HubInfoResponseDto;
 import common.utils.PageUtils.CommonSortBy;
-import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.common.errors.ResourceNotFoundException;
@@ -35,14 +33,15 @@ public class ProductServiceImpl implements ProductService {
     @Transactional
     @Override
     public Product createProduct(ProductCreateRequestDto productCreateRequestDto) {
-        ProductCompanyResponseDto company = companyClient.getCompany(
-            productCreateRequestDto.getCompanyId());
-        ProductHubResponseDto hub = hubClient.getHub(productCreateRequestDto.getHubId());
+        CompanyInfoResponseDto companyInfoResponseDto = companyClient.getCompanyInfoById(
+            productCreateRequestDto.getCompanyId()).getBody();
+        HubInfoResponseDto hubInfoResponseDto = hubClient.getHubInfoById(
+            productCreateRequestDto.getHubId()).getBody();
 
-        if (company == null) {
+        if (companyInfoResponseDto == null) {
             throw new ResourceNotFoundException("Company not found with id: " + productCreateRequestDto.getCompanyId());
         }
-        if (hub == null) {
+        if (hubInfoResponseDto == null) {
             throw new ResourceNotFoundException("Hub not found with id: " + productCreateRequestDto.getHubId());
         }
 
